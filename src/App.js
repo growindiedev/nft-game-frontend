625321906c95207109d6047e268b66659cedc82a
Abhishek Ranjan <rabhishekranjan@gmail.com>
2022-01-16 07:30:25 +0530

    implemented connect wallet feature

--- src/App.js
+++ src/App.js
@@ -1,12 +1,59 @@
-import React from 'react';
-import twitterLogo from './assets/twitter-logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import twitterLogo from "./assets/twitter-logo.svg";
+import "./App.css";
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = "jarryingnut";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  const [currentAccount, setCurrentAccount] = useState(null);
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        console.log("Make sure you have MetaMask!");
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
+        const accounts = await ethereum.request({ method: "eth_accounts" });
+
+        if (accounts.length !== 0) {
+          console.log("Found an authorized account:", accounts[0]);
+          setCurrentAccount(accounts[0]);
+        } else {
+          console.log("No authorized account found");
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const connectWalletAction = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
   return (
     <div className="App">
       <div className="container">
@@ -14,10 +61,22 @@ const App = () => {
           <p className="header gradient-text">⚔️ Metaverse Slayer ⚔️</p>
           <p className="sub-text">Team up to protect the Metaverse!</p>
           <div className="connect-wallet-container">
-            <img
-              src="https://64.media.tumblr.com/tumblr_mbia5vdmRd1r1mkubo1_500.gifv"
+            {/* <img
+              //src="https://64.media.tumblr.com/tumblr_mbia5vdmRd1r1mkubo1_500.gifv"
+              src="https://media3.giphy.com/media/5DfGL75M9spG0/giphy.gif?cid=790b7611c8dc12554841c105294ebf95bc83ea54b95bfd10&rid=giphy.gif&ct=g"
               alt="Monty Python Gif"
+              height={"300vh"}
+            /> */}
+            <img
+              src="https://media.giphy.com/media/d7mMzaGDYkz4ZBziP6/giphy.gif"
+              alt="fight me"
             />
+            <button
+              className="connect-wallet-button cta-button"
+              onClick={connectWalletAction}
+            >
+              Connect wallet to get started
+            </button>
           </div>
         </div>
         <div className="footer-container">
@@ -27,7 +86,7 @@ const App = () => {
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built with @${TWITTER_HANDLE}`}</a>
+          >{`built by @${TWITTER_HANDLE}`}</a>
         </div>
       </div>
     </div>
