47e56f7a0dccc04f65846df66473ddf9e3a5b498
Abhishek Ranjan <rabhishekranjan@gmail.com>
2022-01-17 20:22:20 +0530

    added getCharacters and getContract implimentations

--- src/Components/SelectCharacter/index.js
+++ src/Components/SelectCharacter/index.js
@@ -1,7 +1,52 @@
 import React, { useEffect, useState } from "react";
 import "./SelectCharacter.css";
+import { ethers } from "ethers";
+import { CONTRACT_ADDRESS, transformCharacterData } from "../../constants";
+import myEpicGame from "../../utils/MyEpicGame.json";
 
 const SelectCharacter = ({ setCharacterNFT }) => {
+  const [characters, setCharacters] = useState([]);
+  const [gameContract, setGameContract] = useState(null);
+
+  useEffect(() => {
+    const { ethereum } = window;
+
+    if (ethereum) {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const gameContract = new ethers.Contract(
+        CONTRACT_ADDRESS,
+        myEpicGame.abi,
+        signer
+      );
+
+      setGameContract(gameContract);
+    } else {
+      console.log("Ethereum object not found");
+    }
+  }, []);
+
+  useEffect(() => {
+    const getCharacters = async () => {
+      try {
+        console.log("Getting contract characters to mint");
+        const charactersTxn = await gameContract.getAllDefaultCharacters();
+        console.log("charactersTxn", charactersTxn);
+
+        const characters = charactersTxn.map((CharacterData) =>
+          transformCharacterData(CharacterData)
+        );
+        setCharacters(characters);
+      } catch (error) {
+        console.error("Something went wrong fetching characters:", error);
+      }
+    };
+
+    if (gameContract) {
+      getCharacters();
+    }
+  }, [gameContract]);
+
   return (
     <div className="select-character-container">
       <h2>Mint Your Hero. Choose wisely</h2>
